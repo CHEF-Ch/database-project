a35107c9e538e3f4c1c56c2180e7831652452267
Dale835 <2753046099@qq.com>
2026-09-16 14:45:58 +0800

    Delete zeng-week2.sql

--- sql/week2/zeng-week2.sql
+++
@@ -1,7 +0,0 @@
-create table product
-(		
-id int IDENTITY(1,1) primary key,
-productname nvarchar(50) not null,
-size nvarchar(20),
-price numeric(5,2) not null,
-ListingStatus bit default 1)   --zeng finish
